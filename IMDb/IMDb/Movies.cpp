aaf08381407ec159add70cc90fd82d40cc010f78
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2025-01-12 19:58:55 +0200

    Add case insensitive searching

--- IMDb/IMDb/Movies.cpp
+++ IMDb/IMDb/Movies.cpp
@@ -25,12 +25,14 @@ Response addMovie(const Movie movie)
 bool compareTitle(const char* title, char** movie)
 {
 	// if title is shorter or equal return true
-	return strCmp(title, movie[TITLE_INDEX]) <= 0 && title[0] == movie[TITLE_INDEX][0];
+	return strCaseCmp(title, movie[TITLE_INDEX]) <= 0 && 
+		toLower(title[0]) == toLower(movie[TITLE_INDEX][0]);
 }
 
 bool compareGenre(const char* genre, char** movie)
 {
-	return strCmp(genre, movie[GENRE_INDEX]) <= 0 && genre[0] == movie[GENRE_INDEX][0];
+	return strCaseCmp(genre, movie[GENRE_INDEX]) <= 0 && 
+		toLower(genre[0]) == toLower(movie[GENRE_INDEX][0]);
 }
 
 Movie** getMoviesBy(const char* search,
